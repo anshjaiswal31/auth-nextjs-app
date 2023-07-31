d24010aa03cc2281641716b0504c0cc1b3dfe3f4
ajaiswal473707 <ajaiswal@paymentus.com>
2023-07-31 17:36:41 +0530

    new commit

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,37 @@
+import {connect} from '@/dbConfig/dbConfig'
+import User from '@/models/userModel'
+import { NextRequest, NextResponse } from 'next/server'
+import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+connect()
+
+export async function POST(request:NextRequest) {
+    try {
+        const reqBody=await request.json()
+        const {email,password}=reqBody
+        console.log(reqBody)
+
+        const user=await User.findOne({email})
+        if(!user){
+            return NextResponse.json({error:'User not found'},{status:400})
+        }
+
+        const validpw=await bcryptjs.compare(password,user.password);
+
+        if(!validpw){
+            return NextResponse.json({error:'Incorrect Password'},{status:400})
+        }
+
+        const tokenData={
+            id:user._id,
+            username: user.username
+        }
+
+        const token=await jwt.sign(tokenData,process.env.TOKEN_SECRET)
+        
+    } catch (error:any) {
+        return NextResponse.json({error:error.message},{status:500})
+    }
+    
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,82 @@
+"use client";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from 'axios';
+import toast from 'react-hot-toast';
+
+export default function SignupPage() {
+    const router = useRouter()
+    const [user, setUser] = useState({
+        email: "",
+        password: "",
+        username: ""
+    })
+
+    const [buttonDisabled, setButtonDisabled] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if (user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
+            setButtonDisabled(false);
+        }
+        else {
+            setButtonDisabled(true);
+        }
+    })
+
+    const onSignup = async () => {
+        try {
+            setLoading(true)
+            const response = await axios.post("/api/users/signup", user);
+            toast.success("Signup successful")
+            console.log(response.data)
+            router.push("/login");
+        }
+        catch (error: any) {
+            toast.error(error.message);
+        }
+        finally {
+            setLoading(false);
+        }
+    }
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>{loading ? "Processing" : "Signup"}</h1>
+            <hr />
+            <label htmlFor="username">Username</label>
+            <input
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus-border-gray-600"
+                id="username"
+                type="text"
+                value={user.username}
+                onChange={(e) => setUser({ ...user, username: e.target.value })}
+                placeholder="username"
+            />
+            <label htmlFor="email">Email</label>
+            <input
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus-border-gray-600"
+                id="email"
+                type="text"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+                placeholder="email"
+            />
+            <label htmlFor="password">password</label>
+            <input
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus-border-gray-600"
+                id="password"
+                type="password"
+                value={user.password}
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
+                placeholder="password"
+            />
+            <button className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus-border-gray-600"
+                onClick={onSignup}>
+                {buttonDisabled ? "no signup" : "Signup"}
+            </button>
+            <Link href="/login">Visit Login page</Link>
+        </div>
+    )
+}
